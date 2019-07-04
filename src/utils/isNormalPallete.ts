2a23d20585a5949d7d4cdac719ba412805676f3d
KostiaSA <9675795@gmail.com>
2019-07-04 12:24:55 +0300

    .

--- src/utils/isNormalPallete.ts
+++ src/utils/isNormalPallete.ts
@@ -1,6 +1,6 @@
 import { IResult_wms_android_Паллета_инфо } from "../generated-api";
 
 export function isNormalPallete(pal: IResult_wms_android_Паллета_инфо): boolean {
-    return pal.Организация == 0 && pal.Сотрудник == 0 && pal.Заглушка == 0 && pal.Виртуальная == 0 && pal.ЭтоБрак == 0 && pal.ЭтоДоВыяснения == 0;
+    return pal.Организация == 0 && pal.Сотрудник == 0 && !pal.Заглушка && !pal.Виртуальная && !pal.ЭтоБрак && !pal.ЭтоДоВыяснения;
 
 }
